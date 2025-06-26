68eb426ff52bbf70a39c43d49eac6d48cf18e438
weiihann <weihan774237@gmail.com>
2025-06-26 16:00:31 +0800

    works but slow

--- internal/analyzer.go
+++ internal/analyzer.go
@@ -3,12 +3,14 @@ package internal
 import (
 	"fmt"
 	"log/slog"
+	"runtime"
 	"strconv"
-	"strings"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/hashicorp/golang-lru"
 	"github.com/weiihann/chunk-analysis/internal/logger"
+	"golang.org/x/sync/errgroup"
 )
 
 // Opcode constants for better maintainability
@@ -29,6 +31,7 @@ type Analyzer struct {
 	client    *RpcClient
 	retriever *TraceRetriever
 	log       *slog.Logger
+	codeCache *lru.Cache
 
 	results chan<- *TraceResult
 }
@@ -57,11 +60,17 @@ func newTraceResult(code *Code) *TraceResult {
 	}
 }
 
-func NewAnalyzer(client *RpcClient, retriever *TraceRetriever, results chan<- *TraceResult) *Analyzer {
+func NewAnalyzer(id string, client *RpcClient, retriever *TraceRetriever, results chan<- *TraceResult) *Analyzer {
+	codeCache, err := lru.New(10000)
+	if err != nil {
+		panic(err)
+	}
+
 	return &Analyzer{
 		client:    client,
 		retriever: retriever,
-		log:       logger.GetLogger("analyzer"),
+		log:       logger.GetLogger(fmt.Sprintf("analyzer-%s", id)),
+		codeCache: codeCache,
 		results:   results,
 	}
 }
@@ -72,13 +81,15 @@ func (a *Analyzer) Analyze(blockNum uint64) error {
 		return err
 	}
 
+	var workers errgroup.Group
+	workers.SetLimit(runtime.NumCPU())
 	for _, tx := range trace {
-		if err := a.analyze(&tx, blockNum); err != nil {
-			return err
-		}
+		workers.Go(func() error {
+			return a.analyze(&tx, blockNum)
+		})
 	}
 
-	return nil
+	return workers.Wait()
 }
 
 func (a *Analyzer) analyze(tr *TransactionTrace, blockNum uint64) error {
@@ -105,6 +116,11 @@ func (a *Analyzer) getCodeFromTx(txHash string, blockNum uint64) (*Code, error) 
 }
 
 func (a *Analyzer) getCode(addr string, blockNum uint64) (*Code, error) {
+	cacheKey := codeCacheKey(addr, blockNum)
+	if cached, ok := a.codeCache.Get(cacheKey); ok {
+		return cached.(*Code), nil
+	}
+
 	code, err := a.client.Code(common.HexToAddress(addr), blockNum)
 	if err != nil {
 		return nil, err
@@ -114,10 +130,12 @@ func (a *Analyzer) getCode(addr string, blockNum uint64) (*Code, error) {
 		return nil, err
 	}
 
-	return &Code{
+	result := &Code{
 		addr: addr,
 		code: codeBytes,
-	}, nil
+	}
+	a.codeCache.Add(cacheKey, result)
+	return result, nil
 }
 
 func (a *Analyzer) analyzeSteps(blockNum uint64, code *Code, trace *InnerResult, depth int, counter *int) error {
@@ -132,7 +150,9 @@ func (a *Analyzer) analyzeSteps(blockNum uint64, code *Code, trace *InnerResult,
 	}
 
 	result := newTraceResult(code)
-	for *counter < len(steps) {
+	stepsLen := len(steps)
+
+	for *counter < stepsLen {
 		step := steps[*counter]
 
 		// We detect that we went back to the previous depth, so this is the end of the current depth
@@ -140,12 +160,18 @@ func (a *Analyzer) analyzeSteps(blockNum uint64, code *Code, trace *InnerResult,
 			break
 		}
 
-		result.Bits.Set(uint32(step.PC))
+		if len(code.code) == 0 {
+			*counter++
+			continue
+		}
 
-		switch op := step.Op; {
+		result.Bits.Set(uint32(step.PC))
+		op := step.Op
+
+		switch {
 		case op == OpPush0:
 			// Do nothing
-		case strings.HasPrefix(op, "PUSH"):
+		case len(op) > 4 && op[:4] == "PUSH":
 			if err := a.handlePush(result.Bits, &step); err != nil {
 				return err
 			}
@@ -154,7 +180,8 @@ func (a *Analyzer) analyzeSteps(blockNum uint64, code *Code, trace *InnerResult,
 		case op == OpCodeCopy:
 			result.CodeCopyCount++
 		case op == OpExtCodeSize:
-			code, err := a.getCode(step.Stack[len(step.Stack)-1], blockNum)
+			stackTop := step.Stack[len(step.Stack)-1]
+			code, err := a.getCode(stackTop, blockNum)
 			if err != nil && !trace.Failed {
 				return err
 			}
@@ -164,7 +191,8 @@ func (a *Analyzer) analyzeSteps(blockNum uint64, code *Code, trace *InnerResult,
 				a.results <- extRes
 			}
 		case op == OpExtCodeHash:
-			code, err := a.getCode(step.Stack[len(step.Stack)-1], blockNum)
+			stackTop := step.Stack[len(step.Stack)-1]
+			code, err := a.getCode(stackTop, blockNum)
 			if err != nil && !trace.Failed {
 				return err
 			}
@@ -174,7 +202,8 @@ func (a *Analyzer) analyzeSteps(blockNum uint64, code *Code, trace *InnerResult,
 				a.results <- extRes
 			}
 		case op == OpExtCodeCopy:
-			code, err := a.getCode(step.Stack[len(step.Stack)-1], blockNum)
+			stackTop := step.Stack[len(step.Stack)-1]
+			code, err := a.getCode(stackTop, blockNum)
 			if err != nil && !trace.Failed {
 				return err
 			}
@@ -184,7 +213,8 @@ func (a *Analyzer) analyzeSteps(blockNum uint64, code *Code, trace *InnerResult,
 				a.results <- extRes
 			}
 		case op == OpDelegateCall || op == OpCall || op == OpCallCode || op == OpStaticCall:
-			code, err := a.getCode(step.Stack[len(step.Stack)-2], blockNum)
+			stackSecond := step.Stack[len(step.Stack)-2]
+			code, err := a.getCode(stackSecond, blockNum)
 			if err != nil && !trace.Failed {
 				return err
 			}
@@ -205,17 +235,21 @@ func (a *Analyzer) analyzeSteps(blockNum uint64, code *Code, trace *InnerResult,
 
 // PUSHX opcodes also access the bytecode, add it to the result accordingly
 func (a *Analyzer) handlePush(bits *BitSet, step *TraceStep) error {
-	pushNum := step.Op[len("PUSH"):] // Extract the PUSHN number
+	pushNum := step.Op[4:] // Extract the PUSHN number (skip "PUSH")
 	pushNumInt, err := strconv.Atoi(pushNum)
 	if err != nil {
 		return err
 	}
-	for i := range pushNumInt {
-		res, err := bits.SetWithCheck(uint32(step.PC) + 1 + uint32(i))
-		if err != nil {
+
+	pc := uint32(step.PC)
+	for i := 0; i < pushNumInt; i++ {
+		if _, err := bits.SetWithCheck(pc + 1 + uint32(i)); err != nil {
 			return err
 		}
-		bits = res
 	}
 	return nil
+}
+
+func codeCacheKey(addr string, blockNum uint64) string {
+	return fmt.Sprintf("%s:%d", addr, blockNum)
 }
