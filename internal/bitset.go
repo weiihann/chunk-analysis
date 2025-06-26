affe5c920c6880a4d67fd9995e416f0c37ee92a6
weiihann <weihan774237@gmail.com>
2025-06-26 17:37:00 +0800

    add more optimizations for bitset

--- internal/bitset.go
+++ internal/bitset.go
@@ -11,8 +11,9 @@ const maxContractBytes = 24576
 // Only represent up to 24,576 bytes because that's the current max contract size.
 // It is in big endian order. Least significant bit is the first byte.
 type BitSet struct {
-	bits []uint32
-	size uint32 // Contract size in bytes
+	bits     []uint32
+	size     uint32 // Contract size in bytes
+	setCount uint32
 }
 
 func NewBitSet(size uint32) *BitSet {
@@ -37,13 +38,41 @@ func (b *BitSet) Set(index uint32) *BitSet {
 
 	wordIndex := index / 32
 	bitIndex := index % 32
-	b.bits[wordIndex] |= 1 << bitIndex
+
+	mask := uint32(1 << bitIndex)
+	if b.bits[wordIndex]&mask == 0 {
+		b.setCount++
+	}
+
+	b.bits[wordIndex] |= mask
 
 	return b
 }
 
+func (b *BitSet) SetWithCheck(index uint32) (*BitSet, error) {
+	if index >= b.size {
+		return nil, fmt.Errorf("index out of range (%d >= %d)", index, b.size)
+	}
+
+	wordIndex := index / 32
+	bitIndex := index % 32
+
+	mask := uint32(1 << bitIndex)
+	if b.bits[wordIndex]&mask == 0 {
+		b.setCount++
+	}
+
+	b.bits[wordIndex] |= mask
+
+	return b, nil
+}
+
 // Count the number of set bits in the BitSet
 func (b *BitSet) Count() int {
+	if b.setCount != 0 {
+		return int(b.setCount)
+	}
+
 	count := 0
 	for _, word := range b.bits {
 		count += bits.OnesCount32(word)
@@ -70,4 +99,20 @@ func (b *BitSet) ChunkCount() int {
 // Get the proportion of the contract that was accessed.
 func (b *BitSet) ChunkProportion() float64 {
 	return float64(b.ChunkCount()) / float64(len(b.bits))
+}
+
+func (b *BitSet) Merge(other *BitSet) *BitSet {
+	if b.size != other.size {
+		panic("size mismatch")
+	}
+
+	for i := range b.bits {
+		b.bits[i] |= other.bits[i]
+	}
+
+	return b
+}
+
+func (b *BitSet) IsFull() bool {
+	return b.setCount == b.size
 }
